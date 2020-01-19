d958715e7fb0b95500a52ae9d08c0bab93b111f4
@douglasabnovato <douglasabnovato@gmail.com>
2020-01-19 17:19:36 -0300

    nodemon

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('Hello world !');
+    return res.json({ message: 'Hello Omnistack 10.0 !' });
 });
 
 app.listen(3333);
