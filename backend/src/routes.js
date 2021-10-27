7690b07a5f5dd11dfc5b2cb5d3a4b74a8f47e293
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-27 12:06:04 -0300

    adicionando dev com longitude e latitude

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,7 @@ const routes = Router();
 
 routes.post("/devs", async (request, response) => { 
     
-    const { github_username, techs } = request.body;
+    const { github_username, techs, latitude, longitude } = request.body;
 
     const apiResponse = await axios.get(`http://api.github.com/users/${github_username}`);
 
@@ -13,12 +13,18 @@ routes.post("/devs", async (request, response) => {
 
     const techsArray = techs.split(",").map(tech => tech.trim());
 
+    const location = {
+        type: "Point",
+        coordinates: [longitude, latitude],
+    }
+
     const dev = await Dev.create({
         github_username,
         name, 
         avatar_url,
         bio, 
         techs: techsArray,
+        location,
     }) 
 
     return response.json(dev);
