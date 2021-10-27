5d49ea10f145e6c394ea51d8e4674d27a09e8fda
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-27 14:39:29 -0300

    buscar com filtro

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -1,8 +1,14 @@
-const Dev = require("../models/Dev") 
+const Dev = require("../models/Dev")
+const parseStringAsArray = require("../utils/parseStringAsArray"); 
 
 module.exports = {
     async index(request, response){
-        console.log(request.query);
+
+        const { latitude, longitude, techs } = request.query;
+        const techsArray = parseStringAsArray(techs);
+
+        console.log(techsArray)
+
         return response.json({ devs: [] });
     }
 }
