544a6581b5bb329e80906438d496f379fcd9b31f
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-27 17:27:55 -0300

    arquivos de estilização e estrutura em app

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,19 @@
-function App() {
-  return (
-    <>Dev Radar!</>
+import React from "react";
+
+function App(){
+
+  return(
+    <div id="app">
+      <aside>
+        <h4>sidebar lateral</h4> 
+      </aside>
+
+      <main>
+        <h4>devs cadastrados</h4>
+      </main>
+    </div>
   );
+
 }
 
 export default App;
